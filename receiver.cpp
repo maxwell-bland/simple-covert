fa69d684f870b24111bce009131c1ba7ae5a1780
Maxwell Bland <mbland@eng.ucsd.edu>
2019-10-23 17:06:46 -0700

    Initial implementation of send/recv

--- receiver.cpp
+++ receiver.cpp
@@ -1,22 +1,55 @@
 #include"util.hpp"
 
-int read_bit() {
-  unsigned long long foo;
-  unsigned int bar;
-
-  while (1) {if (time_period(get_time(&bar)) == 5) break;}
-
-  unsigned long iters = 0;
-  float avg = 0.0;
-  while (1) {
-    iters++;
-    avg += read_rand(&foo);
-    if (time_period(get_time(&bar)) == 4) break;
+int recv_msg(unsigned char * buf) {
+  unsigned char buf_i = buf;
+  unsigned char c = 0;
+  while (c != 0xFF) {
+    c = c << 1;
+    c |= read_bit();
   }
 
-  avg /= iters;
+  int len = 0
+  while (1) {
+    for (unsigned long i = 0; i < sizeof(unsigned char) * 8; i++) {
+      *buf |= (read_bit() << i);
+    }
 
-  return avg < THRESH;
+    if ((*buf) == 0xFF) {
+      break;
+    }
+
+    if (*buf) {
+      buf++;
+      len++;
+    }
+  }
+  buf = buf_i;
+
+  unsigned int chk = 0;
+  for (int i = 0; i < len - 1; i++) {
+    chk += buf[i];
+  }
+
+  chk &= 0xFF;
+  chk ^= 0xFF;
+  chk++;
+
+  if (chk != buf[len - 1]) {
+    return -1;
+  }
+
+  for (int i = 0; i < len - 1 - sizeof(unsigned int); i++) {
+    printf("%c", buf[i]);
+  }
+
+  unsigned int msg_num = -1;
+  unsigned char * msg_num_p = (unsigned char *) &msg_num;
+  for (int i = len - 1 - sizeof(unsigned int); i < len - 1; i++) {
+    *msg_num_p = buf[i];
+    msg_num_p++;
+  }
+
+  return msg_num;
 }
 
 int main(int argc, char **argv)
@@ -25,34 +58,14 @@ int main(int argc, char **argv)
 
 	printf("Please press enter.\n");
 
+  unsigned char recv_buf[128] = {0};
 	char text_buf[2];
 	fgets(text_buf, sizeof(text_buf), stdin);
 
 	printf("Receiver now listening.\n");
 
   while (1) {
-    unsigned char num_ones = 0;
-    while (num_ones != 0xFF) {
-      num_ones = num_ones << 1;
-      num_ones |= read_bit();
-    }
-
-    unsigned char c = 0;
-    bool listening = true;
-    while (listening) {
-      for (unsigned long i = 0; i < sizeof(unsigned char) * 8; i++) {
-        c |= (read_bit() << i);
-      }
-
-      if (c == 0xFF) {
-        break;
-      }
-
-      if (c) {
-        fprintf(stderr, "%c\n", c);
-        c = 0;
-      }
-    }
+    recv_msg(recv_buf);
   }
 
 	printf("Receiver finished.\n");
--- sender.cpp
+++ sender.cpp
@@ -1,9 +1,49 @@
-#include "util.hpp"
+#include "util.hop"
+
+unsigned int msg_num = 0;
 
 unsigned int next_bit_one(unsigned char * cp) {
   unsigned int res = (*cp) & 1;
   *cp = (*cp) >> 1;
   return res;
+}
+
+void send_char(unsigned char * c) {
+  for (unsigned long i = 0; i < sizeof(unsigned char) * 8; i++) {
+    write_bit(next_bit_one(c));
+  }
+}
+
+void send_message(char * buf) {
+  for (int i = 0; i < 8; i++) {
+    write_bit(1);
+  }
+
+  unsigned char c;
+  unsigned int chk = 0;
+
+  while (*buf != 0) {
+    c = buf[0];
+    send_char(&c);
+    chk += c;
+    buf++;
+  }
+
+  for (int i = 0; i < sizeof(unsigned int); i++) {
+    c = ((unsigned char *)msg_num) + i;
+    send_char(&c)
+    chk += c;
+  }
+
+  chk &= 0xFF;
+  chk ^= 0xFF;
+  chk++;
+  c = chk;
+  send_char(&c);
+
+  for (int i = 0; i < 8; i++) {
+    write_bit(1);
+  }
 }
 
 int main(int argc, char **argv)
@@ -15,27 +55,8 @@ int main(int argc, char **argv)
 	bool sending = true;
 	while (sending) {
 		char text_buf[128];
-    char * text_p = text_buf;
 		fgets(text_buf, sizeof(text_buf), stdin);
-
-    for (int i = 0; i < 8; i++) {
-      write_bit(1);
-    }
-
-		// Put your covert channel code here
-    while (*text_p != 0) {
-
-      unsigned char c = text_p[0];
-      for (unsigned long i = 0; i < sizeof(unsigned char) * 8; i++) {
-        write_bit(next_bit_one(&c));
-      }
-      text_p++;
-    }
-
-    for (int i = 0; i < 8; i++) {
-      write_bit(1);
-    }
-
+    send_message(text_buf);
 	}
 
 	printf("Sender finished.\n");
